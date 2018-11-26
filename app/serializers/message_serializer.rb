aef476ed497a5af3be425661e36aeaba7a62316a
github username <bteng1226@gmail.com>
2018-11-26 16:17:29 -0500

    changed messages relationship, working action cables

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,5 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :context, :user, :created_at
+  attributes :context, :user, :created_at, :classroom_id
   belongs_to :user
-
+  belongs_to :classroom
 end
